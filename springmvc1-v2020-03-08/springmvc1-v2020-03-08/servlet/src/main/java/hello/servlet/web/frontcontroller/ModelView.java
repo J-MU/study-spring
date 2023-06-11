2f90bde12e97ba663591b836707d068a6202872f
J-MU <alsdnrdl001@gmail.com>
2023-06-11 23:58:20 +0900

    update

---
+++ springmvc1-v2020-03-08/springmvc1-v2020-03-08/servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontcontroller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+public class ModelView {
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+
+}
